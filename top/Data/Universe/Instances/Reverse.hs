c0f32a6181ad1ead98b17ee77918539520a9150d
Daniel Wagner <me@dmwit.com>
2014-04-16 11:53:18 -0400

    no really, export classes from instance modules

--- top/Data/Universe/Instances/Reverse.hs
+++ top/Data/Universe/Instances/Reverse.hs
@@ -3,6 +3,7 @@ module Data.Universe.Instances.Reverse (
 	-- | A convenience module that imports the sibling modules @Eq@, @Ord@,
 	-- @Show@, @Read@, and @Traversable@ to provide instances of these classes
 	-- for functions over finite inputs.
+	Eq(..), Ord(..), Show(..), Read(..), Foldable(..), Traversable(..)
 	) where
 
 import Data.Universe.Instances.Eq          ()
