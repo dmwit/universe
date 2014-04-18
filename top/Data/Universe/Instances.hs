33f37b22bd40371bfebbe8bcaed93d26ac116f7e
Daniel Wagner <me@dmwit.com>
2014-04-17 20:06:52 -0400

    add a deprecated backwards-compatibility module to reduce the impact on whatever existing userbase there is

---
+++ top/Data/Universe/Instances.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+                                              -- the extra space is to make the error line up in a pretty way
+module Data.Universe.Instances {-# DEPRECATED " Use `Data.Universe.Instances.Reverse' instead." #-} (
+	-- | A deprecated convenience module. New applications should import
+	-- "Data.Universe.Instances.Reverse" instead.
+	) where
+
+import Data.Universe.Instances.Reverse ()
