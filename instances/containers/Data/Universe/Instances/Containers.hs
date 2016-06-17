a0c926fe3e9fa6811e1243de6c0befd43f58aead
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-06-17 17:12:56 +0300

    universe-instances-containers with Set instances

--- instances/containers/Data/Universe/Instances/Containers.hs
+++ instances/containers/Data/Universe/Instances/Containers.hs
@@ -1,6 +1,30 @@
-module Data.Universe.Instances.Containers (Universe(..), Finite(..)) where
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+-- | Instances for types from @containers@ package.
+--
+-- * @instance (Ord a, 'Universe' a) => 'Universe' (Set a)@
+module Data.Universe.Instances.Containers (
+    -- | Instances of 'Universe' and 'Finite' for built-in types.
+    Universe(..), Finite(..)
+    ) where
 
--- TODO
--- plausible inclusions:
--- instance (Ord a, Universe a) => Universe (Set a) (+Finite)
--- instance (Ord k, Universe k, Universe v) => Universe (Map k v) (+Finite)
+import Data.Universe.Class
+
+import qualified Data.Set as Set
+-- import qualified Data.Map as Map
+
+instance (Ord a, Universe a, Show a) => Universe (Set.Set a) where
+    universe = Set.empty : go universe
+      where
+        go []     = []
+        go (x:xs) = Set.singleton x : inter (go xs)
+          where
+            -- Probably more efficient than using (+++)
+            inter []     = []
+            inter (y:ys) = y : Set.insert x y : inter ys
+
+
+instance (Ord a, Finite a, Show a) => Finite (Set.Set a)
+
+-- This is tricky
+-- instance (Ord k, Universe k, Universe v) => Universe (Map.Map k v)
+--     universe = ...
---
+++ instances/containers/tests/Tests.hs
@@ -0,0 +1,58 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+module Main (main) where
+
+import Data.Set (Set)
+import Test.QuickCheck
+import Data.Universe.Instances.Base ()
+import Data.Universe.Instances.Containers (Universe(..), Finite(..))
+
+import qualified Data.Set as Set
+
+-- | Proxy type
+data P a = P
+
+-------------------------------------------------------------------------------
+-- Universe laws
+-------------------------------------------------------------------------------
+
+universeLaw :: (Eq a, Show a, Arbitrary a, Universe a) => P a -> a -> Property
+universeLaw _ x = counterexample (show x) (elem x universe)
+
+universeProdLaw
+    :: forall a. (Ord a, Show a, Arbitrary a, Universe a)
+    => P a -> NonNegative (Small Int) -> Property
+universeProdLaw _ (NonNegative (Small n)) = label (show $ div n 10) $
+    let n' = min 10 n
+        pfx = take n' universe :: [a]
+    in length pfx === nubLength pfx
+
+universeLaws :: (Ord a, Show a, Arbitrary a, Universe a) => P a -> Property
+universeLaws p = universeLaw p .&&. universeProdLaw p
+
+nubLength :: Ord a => [a] -> Int
+nubLength = Set.size . Set.fromList
+
+-------------------------------------------------------------------------------
+-- Finite laws
+-------------------------------------------------------------------------------
+
+finiteLaw1 :: (Eq a, Show a, Arbitrary a, Finite a) => P a -> a -> Property
+finiteLaw1 _ x = counterexample (show x) (elem x universeF)
+
+finiteLaw2 :: (Eq a, Show a, Arbitrary a, Finite a) => P a -> a -> Property
+finiteLaw2 _ x = length (filter (== x) universeF) === 1
+
+finiteLaws :: (Ord a, Show a, Arbitrary a, Finite a) => P a -> Property
+finiteLaws p = universeLaws p .&&. finiteLaw1 p .&&. finiteLaw2 p
+
+-------------------------------------------------------------------------------
+-- Main
+-------------------------------------------------------------------------------
+
+main :: IO ()
+main = do
+    quickCheck $ universeProdLaw (P :: P (Set Integer))
+    quickCheck $ finiteLaws (P :: P (Set ()))
+    quickCheck $ finiteLaws (P :: P (Set Bool))
+    quickCheck $ finiteLaws (P :: P (Set (Maybe Bool)))
+    quickCheck $ finiteLaws (P :: P (Set (Set (Maybe Bool))))
