54198f5c72afdc809bf513b0c45031b24975f215
Oleg Grenrus <oleg.grenrus@iki.fi>
2019-02-17 23:07:36 +0200

    Update CI

--- top/Data/Universe.hs
+++ top/Data/Universe.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 module Data.Universe (
-	-- | A convenience module that imports the submodules @Instances.Base@,
+	-- | A convenience module that imports the submodules @Instances.Base@, @Instances.Containers@,
 	-- @Instances.Extended@, and @Instances.Trans@ to provide instances of
 	-- 'Universe' and 'Finite' for a wide variety of types.
 	Universe(..), Finite(..)
 	) where
 
 import Data.Universe.Instances.Base
+import Data.Universe.Instances.Containers
 import Data.Universe.Instances.Extended
 import Data.Universe.Instances.Trans
