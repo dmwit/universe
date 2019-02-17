bbd2b76916491f91955d0278ce7da1b4451e573d
David Feuer <David.Feuer@gmail.com>
2019-02-17 02:30:43 -0500

    Improve Rational instance
    Use a stream
    This feels cleaner to me, but it's not a big deal either way.
    
    Only use the left side of the tree
    
    It turns out that we can save a lot of time and improve sharing by
    building only the traversal of the left side of the Calkin-Wilf tree. We
    can then generate four rational numbers from each node, rather than just
    two. This means we perform fewer sums, so less allocation, and we also
    get more from each pricy pointer lookup.
    
    Note: this commit changes the order of elements in
    `universe @Rational`, but hopefully no one is relying on it
    staying exactly the same forever.
    
    Take the reciprocals manually to avoid unnecessary sign tests.
    
    Add test
    Make the test work
    
    Just throwing `Arbitrary` rationals leads to tests that quickly
    run out of memory. Limit the ranges to avoid this.
    
    Add instances for Natural and Ratio Natural
    
    Now that `Natural` is in `base`, I think both `Natural` and
    `Ratio Natural` deserve `Universe` instances. Add them.

--- universe-base/src/Data/Universe/Class.hs
+++ universe-base/src/Data/Universe/Class.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE CPP, TypeFamilies, ScopedTypeVariables #-}
+{-# LANGUAGE CPP, BangPatterns, TypeFamilies, ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleContexts #-}
 #ifdef DEFAULT_SIGNATURES
 {-# LANGUAGE DefaultSignatures #-}
@@ -22,6 +22,7 @@ import Data.Int (Int, Int8, Int16, Int32, Int64)
 import Data.List (genericLength)
 import Data.Map ((!), fromList)
 import Data.Ratio (Ratio, numerator, denominator, (%))
+import GHC.Real (Ratio (..))
 import Data.Void (Void)
 import Data.Word  (Word, Word8, Word16, Word32, Word64)
 import Numeric.Natural (Natural)
@@ -91,6 +92,7 @@ instance Universe Bool     where universe = universeDef
 instance Universe Char     where universe = universeDef
 instance Universe Ordering where universe = universeDef
 instance Universe Integer  where universe = [0, -1..] +++ [1..]
+instance Universe Natural  where universe = [0..]
 instance Universe Int      where universe = universeDef
 instance Universe Int8     where universe = universeDef
 instance Universe Int16    where universe = universeDef
@@ -121,6 +123,10 @@ instance Universe a => Universe (Mon.Dual    a) where universe = map Mon.Dual   
 instance Universe a => Universe (Mon.First   a) where universe = map Mon.First   universe
 instance Universe a => Universe (Mon.Last    a) where universe = map Mon.Last    universe
 
+-------------------------------------------------------------------------------
+-- Rational
+-------------------------------------------------------------------------------
+
 -- see http://mathlesstraveled.com/2008/01/07/recounting-the-rationals-part-ii-fractions-grow-on-trees/
 --
 -- also, Brent Yorgey writes:
@@ -131,24 +137,69 @@ instance Universe a => Universe (Mon.Last    a) where universe = map Mon.Last   
 --     next x = let (n,y) = properFraction x in recip (fromInteger n + 1 - y)
 --     iterate' f x = let x' = f x in x' `seq` (x : iterate' f x')
 --
--- Compiling this code with -O2 and doing some informal tests seems to
--- show that positiveRationals and positiveRationals2 have almost exactly
--- the same efficiency for generating the entire list (e.g. the times for
--- finding the sum of the first 100000 rationals are pretty much
--- indistinguishable).  positiveRationals is still the clear winner for
--- generating just the nth rational for some particular n -- some simple
--- experiments seem to indicate that doing this with positiveRationals2
--- scales linearly while with positiveRationals it scales sub-linearly,
--- as expected.
+-- But this turns out to be substantially slower.
 --
--- Surprisingly, replacing % with :% in positiveRationals seems to make
--- no appreciable difference.
-positiveRationals :: [Ratio Integer]
-positiveRationals = 1 : map lChild positiveRationals +++ map rChild positiveRationals where
-  lChild frac = numerator frac % (numerator frac + denominator frac)
-  rChild frac = (numerator frac + denominator frac) % denominator frac
+-- We used to use
+--
+--    positiveRationals =
+--      1 : map lChild positiveRationals +++ map rChild positiveRationals
+--
+-- where lChild and rChild produced the left and right child of each fraction,
+-- respectively. Aside from building unnecessary thunks (thanks to the lazy
+-- map), this had the problem of calculating each sum at least four times:
+-- once for a denominator, a second time for the following numerator, and then two
+-- more times on the other side of the Calkin-Wilf tree. That doesn't
+-- sound too bad, since in practice the integers will be small. But taking each
+-- sum allocates a constructor to wrap the result, and that's not
+-- free. We can avoid the problem with very little additional effort by
+-- interleaving manually. Negative rationals, unfortunately, don't get the
+-- full benefit of sharing here, but we can still share their denominators.
 
-instance a ~ Integer => Universe (Ratio a) where universe = 0 : map negate positiveRationals +++ positiveRationals
+infixr 5 :<
+data Stream a = !a :< Stream a
+
+-- All the rational numbers on the left side of the Calkin-Wilf tree,
+-- in breadth-first order.
+leftSideStream :: Integral a => Stream (Ratio a)
+leftSideStream = 1 :% 2 :< go leftSideStream
+  where
+      go (x :< xs) = lChild :< rChild :< go xs
+        where
+          nd = numerator x + denominator x
+          !lChild = numerator x :% nd
+          !rChild = nd :% denominator x
+
+instance RationalUniverse a => Universe (Ratio a) where
+  universe = rationalUniverse
+
+class RationalUniverse a where
+  rationalUniverse :: [Ratio a]
+
+instance RationalUniverse Integer where
+    -- Why force the negations and reciprocals? This is more expensive if we
+    -- ignore most of the result: it allocates four words (generally) for a
+    -- negative element rather than two words for a thunk that will evaluate to
+    -- one. But it's presumably more common to use the elements in a universe
+    -- than to leap over them, so we optimize for the former case. We
+    -- interleave manually to avoid allocating four intermediate lists.
+    rationalUniverse = 0 : 1 : (-1) : go leftSideStream
+      where
+        go (x@(xn :% xd) :< xs) =
+          let !nx = -x
+              !rx = xd :% xn
+              !nrx = -rx
+          in x : rx : nx : nrx : go xs
+
+instance RationalUniverse Natural where
+    rationalUniverse = 0 : 1 : go leftSideStream
+      where
+        go (x@(xn :% xd) :< xs) =
+          let !rx = xd :% xn
+          in x : rx : go xs
+
+-------------------------------------------------------------------------------
+--
+-------------------------------------------------------------------------------
 
 -- could change the Ord constraint to an Eq one, but come on, how many finite
 -- types can't be ordered?
@@ -213,7 +264,7 @@ instance (Finite a, Finite b, Finite c, Finite d, Finite e) => Finite (a, b, c, 
 
 instance Finite Mon.All where universeF = map Mon.All universeF; cardinality = 2
 instance Finite Mon.Any where universeF = map Mon.Any universeF; cardinality = 2
-instance Finite a => Finite (Mon.Sum     a) where universeF = map Mon.Sum     universeF; cardinality = retagWith Mon.Sum     cardinality 
+instance Finite a => Finite (Mon.Sum     a) where universeF = map Mon.Sum     universeF; cardinality = retagWith Mon.Sum     cardinality
 instance Finite a => Finite (Mon.Product a) where universeF = map Mon.Product universeF; cardinality = retagWith Mon.Product cardinality
 instance Finite a => Finite (Mon.Dual    a) where universeF = map Mon.Dual    universeF; cardinality = retagWith Mon.Dual    cardinality
 instance Finite a => Finite (Mon.First   a) where universeF = map Mon.First   universeF; cardinality = retagWith Mon.First   cardinality
--- universe-base/tests/Tests.hs
+++ universe-base/tests/Tests.hs
@@ -6,6 +6,8 @@ import Data.Int (Int8)
 import Test.QuickCheck
 import Data.Universe.Class (Universe(..), Finite(..))
 import Data.Set (Set)
+import Data.Ratio (Ratio, (%))
+import Numeric.Natural (Natural)
 
 import qualified Data.Set as Set
 
@@ -30,6 +32,21 @@ nubLength = Set.size . Set.fromList
 
 universeLaws :: (Ord a, Show a, Arbitrary a, Universe a) => P a -> Property
 universeLaws p = universeLaw p .&&. universeProdLaw p
+
+rationalLaw :: Gen Property
+-- We have to keep the numbers fairly small here to avoid needing to
+-- dig too deep.
+rationalLaw = do
+  n <- choose (-19, 19 :: Integer)
+  d <- choose (1, 19)
+  return $ let nd = n % d in counterexample (show nd) (elem nd universe)
+
+natRatioLaw :: Gen Property
+natRatioLaw = do
+  n <- choose (0, 19 :: Int)
+  d <- choose (1, 19 :: Int)
+  return $ let nd = (fromIntegral n :: Natural) % fromIntegral d
+           in counterexample (show nd) (elem nd universe)
 
 -------------------------------------------------------------------------------
 -- Finite laws
@@ -64,6 +81,11 @@ main = do
     -- i.e. it takes lots of time to get to small numbers!
     quickCheck eitherExample
     quickCheck $ universeLaws (P :: P Integer)
+    quickCheck $ universeLaws (P :: P Natural')
+    quickCheck $ rationalLaw
+    quickCheck $ natRatioLaw
+    quickCheck $ universeProdLaw (P :: P Rational)
+    quickCheck $ universeProdLaw (P :: P (Ratio Natural))
     quickCheck $ finiteLaws (P :: P Char)
     quickCheck $ finiteLaws (P :: P (Maybe Int8))
     quickCheck $ finiteLaws (P :: P (Either Int8 Int8))
@@ -75,3 +97,12 @@ main = do
     quickCheck $ finiteLaws (P :: P (Set Bool))
     quickCheck $ finiteLaws (P :: P (Set (Maybe Bool)))
     quickCheck $ finiteLaws (P :: P (Set (Set (Maybe Bool))))
+
+-------------------------------------------------------------------------------
+-- Natural'
+-------------------------------------------------------------------------------
+
+newtype Natural' = Natural' Natural deriving (Eq, Ord, Show)
+instance Universe Natural' where universe = map Natural' universe
+instance Arbitrary Natural' where
+    arbitrary = fmap (Natural' . fromInteger . abs) arbitrary
