f170c4feb6220d5836b4ea31f16c3b6f8f8e53b2
Oleg Grenrus <oleg.grenrus@iki.fi>
2019-02-17 23:38:52 +0200

    Make cabal check happy

--- instances/reverse/Data/Universe/Instances/Eq.hs
+++ instances/reverse/Data/Universe/Instances/Eq.hs
@@ -1,11 +1,11 @@
 module Data.Universe.Instances.Eq (
-	-- | An 'Eq' instance for functions, given the input is 'Finite' and the
-	-- output is 'Eq'. Compares pointwise.
-	Eq(..)
-	) where
+    -- | An 'Eq' instance for functions, given the input is 'Finite' and the
+    -- output is 'Eq'. Compares pointwise.
+    Eq(..)
+    ) where
 
 import Data.Monoid
 import Data.Universe.Instances.Base
 
 instance (Finite a, Eq b) => Eq (a -> b) where
-	f == g = and [f x == g x | x <- universeF]
+    f == g = and [f x == g x | x <- universeF]
