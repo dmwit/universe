9d7d72f60c381ed48eac57d0433d81c427d37bf4
Oleg Grenrus <oleg.grenrus@iki.fi>
2021-01-09 21:25:36 +0200

    Deprecate universe-dependent-sum

--- universe-dependent-sum/src/Data/Universe/DependentSum.hs
+++ universe-dependent-sum/src/Data/Universe/DependentSum.hs
@@ -1,45 +1,5 @@
-{-# LANGUAGE CPP #-}
-{-# LANGUAGE PackageImports #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-module Data.Universe.DependentSum () where
-
-#if MIN_VERSION_dependent_sum(0,7,0)
+-- | This module re-exports "Data.Universe.Some" from @universe-some@ package.
+module Data.Universe.DependentSum
+{-# DEPRECATED "Use universe-some package directly" #-}
+() where
 import Data.Universe.Some ()
-#else
-
-
-import Data.Universe.Class (Universe (..), Finite (..))
-import Data.Universe.Some (UniverseSome (..), FiniteSome (..))
-import Data.Universe.Helpers (Tagged (..), Natural, (+++))
-
-import           "some"          Data.Some (Some (..), foldSome)
-import qualified "dependent-sum" Data.Some as DS
-
--------------------------------------------------------------------------------
--- Helpers
--------------------------------------------------------------------------------
-
-#if MIN_VERSION_dependent_sum(0,5,0)
-mkSome :: f a -> DS.Some f
-mkSome = DS.Some
-#else
-mkSome :: f a -> DS.Some f
-mkSome = DS.This
-#endif
-
--------------------------------------------------------------------------------
--- Instances for Some
--------------------------------------------------------------------------------
-
-instance UniverseSome f => Universe (DS.Some f) where
-  universe = map (foldSome mkSome) universeSome
-
-instance FiniteSome f => Finite (DS.Some f) where
-  universeF   = map (foldSome mkSome) universeFSome
-  cardinality = retagSome cardinalitySome
-
-retagSome :: Tagged (Some f) Natural -> Tagged (some f) Natural
-retagSome (Tagged n) = Tagged n
-
-#endif
