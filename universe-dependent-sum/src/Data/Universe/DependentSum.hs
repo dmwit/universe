7d2252e5ca17847a58ae56545fa7e7dac5e64856
Oleg Grenrus <oleg.grenrus@iki.fi>
2026-04-16 01:03:06 +0300

    Remove universe-dependent-sum
    It has been deprecated for over six years now.

--- universe-dependent-sum/src/Data/Universe/DependentSum.hs
+++
@@ -1,5 +0,0 @@
--- | This module re-exports "Data.Universe.Some" from @universe-some@ package.
-module Data.Universe.DependentSum
-{-# DEPRECATED "Use universe-some package directly" #-}
-() where
-import Data.Universe.Some ()
